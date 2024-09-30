0d004a657a2d597f757d631dbc7c00747bb5177b
Zaldua-con-Z <camila.zaldua@ucuenca.edu.ec>
2024-09-30 16:37:02 -0500

    Se añadio color en las figuras de las condicionales

--- sketch.js
+++ sketch.js
@@ -1,15 +1,17 @@
-let xCirculo=500
-let yCirculo=500
-let diametro=250
-let esTriangulo= false
+let xCirculo = 500;
+let yCirculo = 500;
+let diametro = 250;
+let esTriangulo = false;
+let colorFigura; 
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-
+    colorFigura = color(random(255), random(255), random(255)); 
 }
 
-function draw (){
+function draw() {
     background(0);
+    fill(colorFigura);
     if (esTriangulo) {
         triangle(
             xCirculo, yCirculo - diametro / 2,
@@ -25,10 +27,14 @@ function mousePressed() {
     let distancia = dist(mouseX, mouseY, xCirculo, yCirculo);
     if (distancia < diametro / 2) {
         esTriangulo = !esTriangulo;
-        xCirculo = random(diametro / 2, width - diametro / 2); 
-        yCirculo = random(diametro / 2, height - diametro / 2); 
+
+        xCirculo = random(diametro / 2, width - diametro / 2);
+        yCirculo = random(diametro / 2, height - diametro / 2);
+        
+        colorFigura = color(random(255), random(255), random(255));
     }
 }
+
 
 
 
